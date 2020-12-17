0c8abb11a2de385176447ec12bd98b79c2a2d179
Virginia202 <virgisonie20@gmail.com>
2020-12-17 12:49:28 +0300

    created the Recipients model

--- bloodbank/models.py
+++ bloodbank/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from datetime import datetime, timedelta
 # Create your models here.
+
+
+class Recipients(models.Model):
+    medical_facility = models.CharField(max_length=200)
+    date_joined = models.DateTimeField(_('date joined'), auto_now_add=True)
+    location = models.ForeignKey(User,on_delete = models.CASCADE)
+    blood_volume = models.IntegerField(null=True)
+
+    def __str__(self):
+      return self.medical_facility
+
+    def create_recipient(self):
+        self.save()
+
+    def delete_recipient(self):
+        self.delete()
