055659ee4e7254e20e279de8e30f2ebcc4dc51f5
Kevin <kevinambundo3@gmail.com>
2020-12-17 16:04:03 +0300

    Add classes

--- bloodbank/models.py
+++ bloodbank/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime, timedelta
 
-# Create your models here.
+
+class Condition(models.Model):
+    condition_name = models.Charfield(max_length=200)
+    description = models.Charfield(max_length=200)
+    other_details = models.Charfield(max_length=200)
+
+    def __str__(self):
+      return self.condtion_name
+
+
+class Donations(models.Model):
+  user = models.OneToOneField(User, on_delete=models.CASCADE)
+  facility = models.Charfield(max_length=200)
+  donate_date = models.DateTimeField(auto_now_add=True)
+  last_donate_date = models.DateField(auto_now_add=True)
+  location = models.Charfield(max_length=50)
+  blood_group = models.Charfield(max_length=50)
+  medical_condition = models.ForeignKey(Donors, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return self.blood_group
