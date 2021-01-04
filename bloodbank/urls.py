811a019bf943e6994415bf4b0c55027e451cba20
martinsmandina <martinsmandina@gmail.com>
2021-01-04 15:56:31 +0300

    create URLcofig profile

--- bloodbank/urls.py
+++ bloodbank/urls.py
@@ -1,5 +1,11 @@
-from django.urls import path
+from django.urls import path, re_path
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
+from .views import ProfileList
+
 
 urlpatterns = [
+    path('api/v1/profile/', views.ProfileList.as_view(), name='edit profile'),
 
 ]
--- bloodbank/views.py
+++ bloodbank/views.py
@@ -1,10 +1,10 @@
 from .models import Profile,BloodStock
-from .serializer import ProfileSerializer,BloodStockSerializer,
+from .serializer import ProfileSerializer,BloodStockSerializer
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404, HttpResponseRedirect
-from .permissions import IsSuperuser, IsActivatedOrReadOnly,IsAdmin
+# from .permissions import IsSuperuser, IsActivatedOrReadOnly,IsAdmin
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated, AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
