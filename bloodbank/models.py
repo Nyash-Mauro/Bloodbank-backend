66a9d4d2847d3751788de6f8e044205cc4b591c9
martinsmandina <martinsmandina@gmail.com>
2021-01-07 10:56:34 +0300

    remove double profile

--- bloodbank/models.py
+++ bloodbank/models.py
@@ -99,34 +99,6 @@ class Condition(models.Model):
     def __str__(self):
       return self.condtion_name
       
-class Profile(models.Model):
-    # user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
-    first_name = models.CharField(max_length=50)
-    middle_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    email = models.EmailField(max_length=255,null=True)
-    age = models.IntegerField(null=True, blank=True)
-    gender = models.CharField(max_length=15)
-    date_of_birth = models.DateField()
-    blood_group = models.CharField(max_length=3)
-    phone_number = models.IntegerField(unique=True)
-    location = models.CharField(max_length=50)
-    weight = models.IntegerField(null=True, blank=True)
-    date_registered = models.DateTimeField(auto_now = True)
-
-    def __str__(self):
-        return self.email
-
-    def save_profile(self):
-        self.save()
-
-    def email_update(self, email):
-        self.email = email
-        self.save_profile()
-
-    def delete_profile(self):
-        self.delete()
-
 class Donations(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     facility = models.CharField(max_length=200)
