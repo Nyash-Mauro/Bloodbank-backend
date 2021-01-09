8da53eff0c8a20670b86e2cd4c2b025308746b61
Virginia202 <virgisonie20@gmail.com>
2021-01-09 18:18:20 +0300

    finaluser endpoint

--- bloodbank/urls.py
+++ bloodbank/urls.py
@@ -15,12 +15,12 @@ urlpatterns = [
     path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/logout/', TokenRefreshView.as_view(), name='logout'),
     path('users/', views.UserViewSet.as_view()),
-    # path('api/register/', RegisterAPI.as_view(), name='register'),
+    path('api/register/', RegisterAPI.as_view(), name='register'),
     # path('api/user/', UserAPI.as_view(), name='user'),
     # path('api/login/', LoginAPI.as_view(), name='login'),
     # path('api/logout/', knox_views.LogoutView.as_view(), name='logout'),
     # path('api/logoutall/', knox_views.LogoutAllView.as_view(), name='logoutall'),
-    # path('api/change-password/', ChangePasswordView.as_view(), name='change-password'),
-    # path('',include(router.urls))
+    path('api/change-password/', ChangePasswordView.as_view(), name='change-password'),
+    path('',include(router.urls))
 
 ]
--- bloodbank/views.py
+++ bloodbank/views.py
@@ -16,79 +16,10 @@ from rest_framework import generics, permissions
 from knox.views import LoginView as KnoxLoginView
 from django.views.decorators.debug import sensitive_post_parameters
 
-# from .email.activation_email import send_activation_email
-
-
-# class RegistrationAPIView(APIView):
-#     permission_classes = (AllowAny,)
-#     renderer_classes = (UserJSONRenderer,)
-#     serializer_class = RegistrationSerializer
-
-#     def post(self, request):
-#         user = request.data.get('user', {})
-#         serializer = self.serializer_class(data=user)
-#         serializer.is_valid(raise_exception=True)
-#         serializer.save()
-
-#         return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-
-# class LoginAPIView(APIView):
-#     permission_classes = (AllowAny,)
-#     renderer_classes = (UserJSONRenderer,)
-#     serializer_class = LoginSerializer
-
-#     def post(self, request):
-#         user = request.data.get('user', {})
-#         serializer = self.serializer_class(data=user)
-#         serializer.is_valid(raise_exception=True)
-
-#         return Response(serializer.data, status=status.HTTP_200_OK)
-
-
-# class UserRetrieveUpdateAPIView(RetrieveUpdateAPIView):
-#     permission_classes = (IsAuthenticated,)
-#     renderer_classes = (UserJSONRenderer,)
-#     serializer_class = UserSerializer
-
-#     def retrieve(self, request, *args, **kwargs):
-#         serializer = self.serializer_class(request.user)
-
-#         return Response(serializer.data, status=status.HTTP_200_OK)
-
-#     def update(self, request, *args, **kwargs):
-#         serializer_data = request.data.get('user', {})
-
-#         serializer = self.serializer_class(
-#             request.user, data=serializer_data, partial=True
-#         )
-#         serializer.is_valid(raise_exception=True)
-#         serializer.save()
-
-#         return Response(serializer.data, status=status.HTTP_200_OK)
-
-
-# class UserViewSet(viewsets.ModelViewSet):
-#     """
-#     A viewset for  user instances.
-#     """
-#     serializer_class = UserSerializer
-#     queryset = User.objects.all()
-
 
 
 class RegisterAPI(generics.GenericAPIView):
     serializer_class = RegisterSerializer
-
-    # def post(self, request, *args, **kwargs):
-    #     serializer = self.get_serializer(data=request.data)
-    #     serializer.is_valid(raise_exception=True)
-    #     user = serializer.save()
-    #     return Response({
-    #     "user": UserSerializer(user, context=self.get_serializer_context()).data,
-    #     "token": AuthToken.objects.create(user)[1]
-    #     })
-
     def post(self, request, format=None):
         print(request.data)
         serializers = UserSerializer(data=request.data)
